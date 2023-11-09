c0b167d4f42b9560afbbadbe1ec4fc0d990a0d33
Victor Escandiel <joaoescandielb@gmail.com>
2023-11-09 15:49:03 -0300

    AboutMe page almost done; banner, title, img, par, all done

--- src/pages/AboutMe/index.jsx
+++ src/pages/AboutMe/index.jsx
@@ -1,3 +1,13 @@
+import ModelPost from "components/ModelPost";
+import styles from "./AboutMe.module.css";
+import bannerPhoto from "assets/sobre_mim_capa.png";
+import aboutMePhoto from "assets/sobre_mim_foto.png";
+
 export default function AboutMe() {
-  return <h1>About me</h1>;
+  return (
+    <ModelPost fotoCapa={bannerPhoto} titulo="Sobre mim">
+      <h3 className={styles.subtitulo}> oi eu sou o escandi</h3>
+      <img src={aboutMePhoto} alt="" className={styles.fotoSobreMim} />
+    </ModelPost>
+  );
 }
