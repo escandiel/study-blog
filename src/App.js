64931a0c51f6ab5cc2bc0b0af6385d2c23a57305
Victor Escandiel <joaoescandielb@gmail.com>
2023-11-08 21:53:49 -0300

    Route imported to App.js file from the react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import { BrowserRouter, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AboutMe from "./pages/about-me";
 import Home from "./pages/home";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes></Routes>
+      <Routes>
+        <Route />
+      </Routes>
     </BrowserRouter>
   );
 }
