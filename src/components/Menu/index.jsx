41634a1701d26efec5326595007571bed657c0e4
Victor Escandiel <joaoescandielb@gmail.com>
2023-11-08 23:09:24 -0300

    importing and using the NavLink component from react-router-dom

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,12 +1,18 @@
 import styles from "./Menu.module.css";
 import MenuLink from "../MenuLink";
+import { NavLink } from "react-router-dom";
 
 export default function Menu() {
   return (
     <header>
       <nav className={styles.navegacao}>
-        <MenuLink to={"/"}>Inicio</MenuLink>
-        <MenuLink to={"/sobremim"}>Sobre Mim</MenuLink>
+        <NavLink exact to={"/"} className={styles.link}>
+          <MenuLink to={"/"}>Inicio</MenuLink>
+        </NavLink>
+        <NavLink exact to={"/sobremim"} className={styles.link}>
+        <MenuLink to={"/sobremim"}>Sobre Mim</MenuLink>  
+        </NavLink>
+        
       </nav>
     </header>
   );
