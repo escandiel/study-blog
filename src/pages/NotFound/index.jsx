1e97306a4e009113cb44ae47b06c1f68f2d08ca2
Victor Escandiel <joaoescandielb@gmail.com>
2023-11-11 11:32:23 -0300

    refactoring the button and creating a buttonMain component

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,3 +1,4 @@
+import ButtonMain from "components/ButtonMain";
 import styles from "./NotFound.module.css";
 import erro404 from "assets/erro_404.png";
 
@@ -12,17 +13,13 @@ export default function NotFound() {
           {" "}
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas,
           nulla! Corrupti iusto mollitia nisi sed dolore ab delectus numquam
-          magni neque repellendus ratione voluptatibus expedita itaque velit,
-          eaque reiciendis impedit.
         </p>
         <p className={styles.paragrafo}>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
           obcaecati laboriosam sequi ad explicabo consequatur rem facilis
-          aliquam molestias, placeat architecto ducimus repellendus atque
-          nesciunt voluptates ipsum similique ut nostrum.
         </p>
         <div className={styles.botaoContainer}>
-          <button>Voltar</button>
+          <ButtonMain size="lg">Voltar</ButtonMain>
         </div>
         <img
           className={styles.imagemCachorro}
